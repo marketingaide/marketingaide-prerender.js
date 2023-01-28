ecf9b4bbc9b369f9cb97fae80f9b96078207f659
marketingai.de <123821247+marketingaide@users.noreply.github.com>
2023-01-28 09:18:03 -0600

    Add files via upload

---
+++ marketingaide-buttons.js
@@ -0,0 +1,1 @@
+buttonRender=()=>{var x=document.createElement("link");x.setAttribute("rel","prerender");x.setAttribute("href",location.href);document.head.appendChild(x);};document.getElementById("mybutton").onmouseover=()=>{buttonRender()};document.getElementById("mybutton").ontouchstart=()=>{buttonRender()};
